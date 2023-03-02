8aba2a7b5bd1e07fb619a20a130eddbb82575bcc
Ahmet Gundogdu <agundogdu99@gmail.com>
2023-03-02 21:52:26 +0000

    rps initial implemantation

---
+++ Other projects/Python/Rock Paper Scissors/RPS.py
@@ -0,0 +1,48 @@
+import random
+from time import sleep
+
+print("Welcome to Ahmet's Rock Paper Scissors Game...")
+
+rounds = int(input("How many rounds would you like to play? (Between 1 - 10)"))
+counter = 0
+
+
+def userSelection():
+    try:
+        noSelection = True
+        while noSelection:
+            userChoice = input(
+                "What is your choice?\nType in...\n1 for Rock\n2 for Paper\n3 for Scissors\nOr press 9 to exit\n"
+            )
+            if userChoice == '9':
+                print("Exiting...")
+                noSelection = False
+                break
+            elif userChoice != '1' or userChoice != '2' or userChoice != '3':
+                print("You must enter a valid selection...")
+                sleep(1)
+                userSelection()
+
+            else:
+                if userChoice == '1':
+                    return 1
+                elif userChoice == '2':
+                    return 2
+                elif userChoice == '3':
+                    return 3
+                else:
+                    print("Error...")
+                noSelection = False
+    except:
+        pass
+
+
+userSelection()
+# def randomGenerate():
+#     randomNum = random.randint(1, 3)
+#     if randomNum == 1:
+#         return 'Rock'
+#     elif randomNum == 2:
+#         return 'Paper'
+#     else:
+#         return 'Scissors'
